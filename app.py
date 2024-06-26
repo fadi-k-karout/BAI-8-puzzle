a85d1437c28c18cb8d84f60a74e45dd100d622f1
Fadi Karout <97478111+fadi-k-karout@users.noreply.github.com>
2024-06-26 19:05:00 +0000

    routes (index, solve)

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+from algorithms.a_star import a_star_search, get_solution_path
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/solve', methods=['POST'])
+def solve():
+    data = request.get_json()
+    start_state = data['start']
+    end_state = data['end']
+    solution = a_star_search(start_state, end_state)
+    if solution:
+        print(solution)
+        path = get_solution_path(solution)
+        return jsonify({"solution": path})
+    else:
+        return jsonify({"solution": None}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
